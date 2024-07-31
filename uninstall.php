e8545b9a25aee2e37813dd2e2c99b84b2cac801f
m.m <irmmr.ir@gmail.com>
2024-08-01 00:06:00 +0330

    update uninstall

--- uninstall.php
+++ uninstall.php
@@ -13,15 +13,22 @@ defined('WP_UNINSTALL_PLUGIN') || die;
 $config_file = require plugin_dir_path(__FILE__) . 'wpnb-config.php';
 
 use Irmmr\WpNotifBell\Db;
+use Irmmr\WpNotifBell\Logger;
 use Irmmr\WpNotifBell\Settings;
 
 global $wpdb;
+
+// add log for uninstalling
+Logger::add('Uninstalling wpnb');
 
 // check data delete status
 $delete_data = Settings::get('admin.manage.rm_data') === 'yes';
 
 // remove all data
 if ($delete_data) {
+    // add log for removing data
+    Logger::add('Deletion of data is active. Deleting all plugin data');
+
 	// delete all user meta
 	$wpdb->query("DELETE FROM $wpdb->usermeta WHERE meta_key LIKE 'wpnb\_%';");
 
@@ -38,4 +45,10 @@ if ($delete_data) {
     // remove main storage folder
     // The storage folder must be removed manually
     // path  =>   /wp-content/wpnb
+
+    // add log for after delete data
+    Logger::add('The data of the plugin has been deleted');
 }
+
+// add log for uninstalling
+Logger::add('wpnb uninstalled');
