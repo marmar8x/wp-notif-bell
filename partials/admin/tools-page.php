960d3fe437611f25aa285e0d59bd07090cfce4d0
marmar8x <marmar8x.code@gmail.com>
2025-03-03 03:17:19 +0330

    add reset tool

--- partials/admin/tools-page.php
+++ partials/admin/tools-page.php
@@ -12,16 +12,20 @@ defined('WPINC') || die;
 use Irmmr\WpNotifBell\Admin\Utils\Notice;
 use Irmmr\WpNotifBell\Db;
 use Irmmr\WpNotifBell\Helpers\Esc;
+use Irmmr\WpNotifBell\Helpers\Notif;
+use Irmmr\WpNotifBell\Interfaces\UserInterface;
+use Irmmr\WpNotifBell\User\Eye;
 
 /**
  * create tab url for tab btn
- * 
+ *
+ * @since   0.9.0
  * @param   string  $tab
  * @return  string
  */
 function mm8x_make_tab_url(string $tab): string
 {
-    return add_query_arg('tab', $tab);
+    return add_query_arg('tab', $tab, remove_query_arg(['wpnb-process', 'nonce']));
 }
 
 // page notice messages
@@ -34,8 +38,14 @@ $_tab = sanitize_key($_GET['tab'] ?? 'db-update');
 $_tabs = [
     'db-update' => [
         'name' => __('Database Updater', 'notif-bell'),
-    ]
+    ],
+    'reset' => [
+        'name' => __('Reset', 'notif-bell'),
+    ],
 ];
+
+// after process
+$_after_process = false;
 
 // handle process with nonce
 if (isset($_GET['wpnb-process']) && isset($_GET['nonce'])) {
@@ -47,6 +57,33 @@ if (isset($_GET['wpnb-process']) && isset($_GET['nonce'])) {
             Db::update_tables();
 
             $_messages[] = Notice::render(__('The database has been successfully updated.', 'notif-bell'), Notice::SUCCESS);
+            $_after_process = true;
+
+        } elseif ($_process === 'reset') {
+            global $wpdb;
+
+            // reset seen data
+            $key_list   = sanitize_key( UserInterface::SEEN_META_KEY );
+            $key_method = sanitize_key( UserInterface::SEEN_METHOD_KEY );
+            $notifs_tb  = Db::get_table_name('notifs');
+
+            if (empty($key_list) || empty($key_method) || empty($notifs_tb)) {
+                $_messages[] = Notice::render(__('An issue has occurred! Please try again.', 'notif-bell'), Notice::ERROR);
+
+            } elseif (Notif::get_max_id() === 0) {
+                $_messages[] = Notice::render(__('There is absolutely nothing to reset!', 'notif-bell'), Notice::WARN);
+
+            } else {
+                $def_method = sanitize_key( strval( Eye::BY_COMMA ) );
+                $wpdb->query("UPDATE {$wpdb->usermeta} SET `meta_value` = '' WHERE `meta_key` = '{$key_list}';");
+                $wpdb->query("UPDATE {$wpdb->usermeta} SET `meta_value` = '{$def_method}' WHERE `meta_key` = '{$key_method}';");
+
+                // reset notifications
+                $wpdb->query("TRUNCATE {$notifs_tb};");
+
+                $_messages[] = Notice::render(__('Everything has been successfully reset.', 'notif-bell'), Notice::SUCCESS);
+                $_after_process = true;
+            }
         }
     } else {
         $_messages[] = Notice::render(__('The submitted request is not valid.', 'notif-bell'), Notice::ERROR);
@@ -90,7 +127,30 @@ if (isset($_GET['wpnb-process']) && isset($_GET['nonce'])) {
                 <span class="wpnb-txt-success"><?php esc_html_e('The database is up to date.', 'notif-bell'); ?></span>
             <?php else: ?>
                 <span class="wpnb-txt-danger"><?php esc_html_e('The database needs to be updated.', 'notif-bell'); ?></span>
-                <a href="<?php echo esc_url( add_query_arg([ 'wpnb-process' => 'up-db', 'nonce' => wp_create_nonce('prc:up-db') ]) ); ?>"><?php esc_html_e('Update', 'notif-bell'); ?></a>
+
+                <?php if (!$_after_process): ?>
+                    <a href="<?php echo esc_url( add_query_arg([ 'wpnb-process' => 'up-db', 'nonce' => wp_create_nonce('prc:up-db') ]) ); ?>"><?php esc_html_e('Update', 'notif-bell'); ?></a>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+        <?php elseif ($_tab === 'reset'): ?>
+        <div class="tab-content wpnb-ad-l-box wpnb-mx-0">
+            <p>
+                <?php esc_html_e('This tool helps you reset all notification data and the user\'s seen status. It is recommended to start everything over after long-term use and a very high number of notifications (over 10,000).', 'notif-bell'); ?>
+            </p>
+            <?php $max_id = Notif::get_max_id(); ?>
+            <?php esc_html_e('MAX ID', 'notif-bell'); ?>: <b class="wpnb-txt-primary"><?php echo esc_html( $max_id ); ?></b> <br />
+            <?php esc_html_e('Notifs count', 'notif-bell'); ?>: <b class="wpnb-txt-primary"><?php echo esc_html( wpnb_collector()->get_count() ); ?></b> <br />
+
+            <?php if ($max_id < 500*1000): ?>
+                <span class="wpnb-txt-success"><?php esc_html_e('Everything looks good.', 'notif-bell'); ?></span>
+            <?php else: ?>
+                <span class="wpnb-txt-danger"><?php esc_html_e('It\'s better for the data to be reset.', 'notif-bell'); ?></span>
+            <?php endif; ?>
+            <br />
+            <span class="wpnb-txt-danger"><?php esc_html_e('Please note: This action will delete all stored data related to notifications.', 'notif-bell'); ?></span>
+            <?php if (!$_after_process): ?>
+                <a href="<?php echo esc_url( add_query_arg([ 'wpnb-process' => 'reset', 'nonce' => wp_create_nonce('prc:reset') ]) ); ?>"><?php esc_html_e('Reset', 'notif-bell'); ?></a>
             <?php endif; ?>
         </div>
         <?php else: ?>
